3c80efed16bb2d7373c4f2a1b579f419a811c8a5
Szabolcs Veer <futuremove@gmail.com>
2018-10-27 10:47:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
  var copy = Object.assign({}, object);
- copy[key] = object;
+ copy[key] = value;
   return copy;
  
 }
